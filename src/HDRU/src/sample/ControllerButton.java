64b4b7a636fa7a384b1b081a6e5ec3c00191501e
Emir Derouiche <memir0@hotmail.no>
2019-03-04 17:41:13 +0100

    fiksa alt

--- src/HDRU/src/sample/ControllerButton.java
+++ src/HDRU/src/sample/ControllerButton.java
@@ -21,7 +21,12 @@ public class ControllerButton extends ChangeScene{
     public TextField textField;
     public TextField email;
     public TextField password;
-    public Label emailWrong;
+    public TextField user_reg;
+    public TextField email_reg;
+    public TextField birthyear_reg;
+    public TextField pass_reg;
+    public TextField confirm_reg;
+
 
     public void sceneInfo(ActionEvent event) throws IOException { //trykker på infoknapp
        super.change(event, "Info.fxml"); //bruker super-metode
@@ -39,7 +44,7 @@ public class ControllerButton extends ChangeScene{
         super.change(event, "Feedback.fxml"); //bruker super-metode
     }
 
-    public void reg() throws SQLException {
+    public void reg() {
         Connection connection = null;
         Statement statement = null;
         Cleaner cleaner = new Cleaner();
@@ -51,6 +56,7 @@ public class ControllerButton extends ChangeScene{
             String getYear = birthyear_reg.getText();
             birthyear = Integer.parseInt(getYear);
         }
+
         if(pass_reg.getText().equals(confirm_reg.getText())){
             password = pass_reg.getText();
         }
@@ -66,8 +72,10 @@ public class ControllerButton extends ChangeScene{
             connection.close();
         }catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            cleaner.closeStatement(statement);
+            cleaner.closeConnection(connection);
         }
-
     }
 
     public void playerLogin(ActionEvent event) throws IOException{
@@ -78,6 +86,7 @@ public class ControllerButton extends ChangeScene{
 
 		String sql = "SELECT password FROM Player WHERE email ='" + email.getText() + "';";
 		try {
+		    Cleaner cleaner = new Cleaner();
 			Statement statement = connection.createStatement();
             rs = statement.executeQuery(sql);
 
@@ -86,16 +95,16 @@ public class ControllerButton extends ChangeScene{
             String realPassword = rs.getString("password");
 
             if(realPassword.equals(password.getText())){
-                rs.close();
-                statement.close();
-                connection.close();
+                cleaner.closeResult(rs);
+                cleaner.closeStatement(statement);
+                cleaner.closeConnection(connection);
                 super.change(event, "Game.fxml");
             }
             else{
-                rs.close();
-                statement.close();
-                connection.close();
-                super.changeVisibility(true, emailWrong);
+                cleaner.closeResult(rs);
+                cleaner.closeStatement(statement);
+                cleaner.closeConnection(connection);
+                super.change(event, "Info.fxml"); //her skal en pop-up komme
             }
 
 		}catch (Exception e){
