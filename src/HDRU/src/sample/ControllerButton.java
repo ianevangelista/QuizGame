7e7ea59cb1ca5da07acc87305eabe5d048c4d486
iaevange <iaevange@stud.ntnu.no>
2019-03-04 11:06:34 +0100

    Endring

---
+++ src/HDRU/src/sample/ControllerButton.java
@@ -0,0 +1,49 @@
+package sample;
+
+import Connection.ConnectionClass;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class ControllerButton extends ChangeScene{
+
+    @FXML
+    public TextField textField;
+
+    public void sceneInfo(ActionEvent event) throws IOException {
+       super.change(event, "Info.fxml"); //bruker super-metode
+    }
+
+    public void sceneHome(ActionEvent event) throws IOException {
+        super.change(event, "Main.fxml"); //bruker super-metode
+    }
+
+    public void register(ActionEvent event) throws IOException {
+        super.change(event, "Register.fxml"); //bruker super-metode
+    }
+
+    public void feedback(ActionEvent event) throws IOException {
+        super.change(event, "Feedback.fxml"); //bruker super-metode
+    }
+
+    public void reg() {
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection = connectionClass.getConnection();
+
+        String sql = "INSERT INTO navn VALUES(\""+ textField.getText() +"\");";
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+}
