8ceaebdbf318159da6e6c3c54349a35838f9a39b
iaevange <iaevange@stud.ntnu.no>
2019-03-08 08:56:29 +0100

    endring i cleaner

--- src/Connection/Cleaner.java
+++ src/Connection/Cleaner.java
@@ -7,23 +7,38 @@ import java.sql.Statement;
 public class Cleaner {
     public Cleaner(){}
 
-    public void close(Statement statement, ResultSet resultset, Connection connection){
+    public void closeStatement(Statement statement){
         try {
             if (statement != null) statement.close();
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (resultset != null) resultset.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (connection != null) connection.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
         }
+    }
+
+    public void closeResult(ResultSet resultset){
+        try {
+            if (resultset != null) resultset.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection(Connection connection){
+        try {
+            if (connection != null) connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeTwo(Statement statement, Connection connection){
+        closeStatement(statement);
+        closeConnection(connection);
+    }
+
+    public void closeThree(Statement statement, ResultSet resultset, Connection connection){
+        closeStatement(statement);
+        closeResult(resultset);
+        closeConnection(connection);
     }
 }
