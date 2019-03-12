376fdebe50dd18646752af476ac70507d0aa7a80
Emir Derouiche <memir0@hotmail.no>
2019-03-12 16:27:54 +0100

    added comments and some changes

--- src/sample/ChooseOpponent.java
+++ src/sample/ChooseOpponent.java
@@ -15,12 +15,12 @@ import static sample.ControllerHome.getUserName;
 
 public class ChooseOpponent{
 
-    public Label usernameWrong;
+
     private Cleaner cleaner = new Cleaner();
 
     private ConnectionClass connectionClass;
     private Connection connection;
-    private String username;
+    private String username = getUserName();
     private String opponentUsername = null;
     private static int gameId;
 
@@ -28,7 +28,7 @@ public class ChooseOpponent{
     @FXML
     public TextField opponent;
     public Button challenge;
-
+    public Label usernameWrong;
 
     public void findOpponent(ActionEvent event) {
         ResultSet rs = null;
@@ -37,7 +37,6 @@ public class ChooseOpponent{
         try{
             connectionClass = new ConnectionClass();
             connection = connectionClass.getConnection();
-            findMyUsername();
             usernameWrong.setVisible(false);
             String insertSql = "SELECT username FROM Player WHERE username =?;";
             insertSentence = connection.prepareStatement(insertSql);
@@ -58,17 +57,13 @@ public class ChooseOpponent{
         }
     }
 
-    private void findMyUsername() {
-        this.username = getUserName();
-    }
-
     private void makeGame(String player1, String player2) {
         Statement statement = null;
         ResultSet rsGameId = null;
 
         try{
             statement = connection.createStatement();
-            String sqlInsert = "INSERT INTO Game(player1, player2) VALUES('"+ player1 + "', '" + player2 + "');";
+            String sqlInsert = "INSERT INTO Game(player1, player2, p1Points, p2Points) VALUES('"+ player1 + "', '" + player2 + "', 0, 0);";
             statement.executeUpdate(sqlInsert, Statement.RETURN_GENERATED_KEYS);
             rsGameId = statement.getGeneratedKeys();
             rsGameId.next();
