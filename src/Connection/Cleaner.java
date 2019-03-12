49fc3dbc09d70e1ac6fabd87d5bffe5b8670e203
iaevange <iaevange@stud.ntnu.no>
2019-03-12 13:10:44 +0100

    pstmt i controllerregister og endringer i cleaner

--- src/Connection/Cleaner.java
+++ src/Connection/Cleaner.java
@@ -1,6 +1,7 @@
 package Connection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -8,6 +9,26 @@ public class Cleaner {
     public Cleaner(){}
 
     public void close(Statement statement, ResultSet resultset, Connection connection){
+        try {
+            if (statement != null) statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultset != null) resultset.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (connection != null) connection.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void close(PreparedStatement statement, ResultSet resultset, Connection connection){
         try {
             if (statement != null) statement.close();
         } catch (Exception e) {
