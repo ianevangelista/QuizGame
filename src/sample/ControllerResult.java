82a5e4ae456d2bcf25acf970452d0154d019d45c
Emir Derouiche <memir0@hotmail.no>
2019-03-19 13:13:23 +0100

    Fixed textField mixup

--- src/sample/ControllerResult.java
+++ src/sample/ControllerResult.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import javax.swing.*;
 import java.io.IOException;
@@ -31,11 +32,11 @@ public class ControllerResult {
     public Button refresh;
 
     //result
-    public TextField totalScore;
-    public TextField resultText;
+    public Text totalScore;
+    public Text resultText;
     public TextField resultHeading;
 
-    public void sceneHome(ActionEvent event){ ChangeScene.change(event, "Game.fxml");}
+    public void sceneGame(ActionEvent event) { ChangeScene.change(event, "Game.fxml"); }
 
     public void sceneChallengeUser(ActionEvent event){ ChangeScene.change(event, "ChallengeUser.fxml");}
 
@@ -96,7 +97,9 @@ public class ControllerResult {
                 String sqlGetPlayerScore = "SELECT points FROM Player WHERE username = " + username;
                 rs = statement.executeQuery(sqlGetPlayerScore);
                 rs.next();
-                totalScore.setText("Your score is now: " + rs.getInt("points"));
+
+                String points = rs.getInt("points") + "p";
+                totalScore.setText(points);
 
             }
             else{
