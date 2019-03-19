54a8bce340969868b62061e1d185186795ca257c
junill <junill@stud.ntnu.no>
2019-03-19 17:40:34 +0100

    Woho funker!!!!

--- src/sample/ControllerEmail.java
+++ src/sample/ControllerEmail.java
@@ -4,6 +4,7 @@ import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
 
+import Connection.ConnectionPool;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -16,7 +17,7 @@ public class ControllerEmail {
     public TextArea feedback;
     public TextField email;
 
-    public void feedback(ActionEvent event){
+    public void sendFeedback(){
 
         String sendTo = "howdumbru.game@gmail.com";
         String host = "smtp.gmail.com";
@@ -40,7 +41,7 @@ public class ControllerEmail {
             message.setFrom(new InternetAddress(username));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(sendTo));
 
-            message.setSubject("Feedback fra How Dumb R U?");
+            message.setSubject("Feedback from How Dumb R U?");
             message.setText(feedback.getText());
 
             Transport transport = session.getTransport("smtp");
@@ -48,14 +49,57 @@ public class ControllerEmail {
             transport.sendMessage(message, message.getAllRecipients());
             transport.close();
 
-            ChangeScene.change(event, "Main.fxml");
-
         }
         catch (MessagingException e) {
             System.out.println("Heihei fanger feil");
             e.printStackTrace();
         }
 
+    }
+
+    public void sendConfirm(){
+
+        String sendTo = email.getText();
+        String host = "smtp.gmail.com";
+        String username = "howdumbru.game@gmail.com";
+        String password = "Junierkul";
+
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.user", username);
+        props.put("mail.smtp.password", password);
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.ssl.trust", host);
+
+        try{
+
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(sendTo));
+
+            message.setSubject("Received feedback from How Dumb R U?");
+            message.setText("Feedback received! We will come back to you as soon as possible.");
+
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, username, password);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+
+        }
+        catch (MessagingException e) {
+            System.out.println("Heihei fanger feil2");
+            e.printStackTrace();
+        }
+
+    }
+
+    public void feedback(ActionEvent event){
+        sendFeedback();
+        sendConfirm();
+        ChangeScene.change(event, "Main.fxml");
     }
 
     public void sceneHome(ActionEvent event) { //feedback knapp
