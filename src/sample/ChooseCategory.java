a63f7ca6b3c2d0e6ebbd050924b7bd818136c667
iaevange <iaevange@stud.ntnu.no>
2019-03-25 11:03:27 +0100

    removed unnecassry code

--- src/sample/ChooseCategory.java
+++ src/sample/ChooseCategory.java
@@ -3,9 +3,6 @@ package sample;
 import Connection.ConnectionPool;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
