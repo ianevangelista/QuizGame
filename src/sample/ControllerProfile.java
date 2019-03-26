c4e828007537d8f6c3cb96dcbd763275f5df9c69
junill <junill@stud.ntnu.no>
2019-03-26 12:47:46 +0100

    PROFILE FUNKER!!!!!

--- src/sample/ControllerProfile.java
+++ src/sample/ControllerProfile.java
@@ -19,12 +19,7 @@ import static sample.ControllerHome.getUserName;
 
 public class ControllerProfile {
 
-    private static String username = getUserName();
-
-    private byte[] salt1;
-    private String stringSalt;
-    private String password;
-    private boolean checking = false;
+    private String username = getUserName();
 
     private Connection connection = null;
     private PreparedStatement statement = null;
@@ -37,24 +32,9 @@ public class ControllerProfile {
     public Label printWon;
     public Label printLost;
 
-    public Button confirmEmail;
-    public Button confirmGender;
-    public Button confirmPassword;
-    public Label errorMessage;
-    public Label confirmed;
-    public Label wrongPassword;
-    public TextField inputEmail;
-    public PasswordField inputOldPassword;
-    public PasswordField inputNewPassword;
-    public RadioButton btnMale;
-    public RadioButton btnFemale;
-    public ToggleGroup gender;
 
     public void initialize() {
-        if(!checking) {
-            choosePic();
-            checking = true;
-        }
+        choosePic();
     }
 
     public void sceneGame(ActionEvent event) { //hjemknapp
@@ -62,8 +42,8 @@ public class ControllerProfile {
     }
 
     public void edit(ActionEvent event){
-        System.out.println("test");
         ChangeScene.change(event, "EditProfile.fxml");
+        System.out.println("test");
     }
 
     public void choosePic(){
@@ -122,120 +102,5 @@ public class ControllerProfile {
         }
     }
 
-    public void emailConfirm(){
-
-        String input = "UPDATE Player SET email = ? WHERE username = ?";
-
-        try{
-            connection = ConnectionPool.getConnection();
-
-            statement = connection.prepareStatement(input);
-            statement.setString(2,username);
-
-            if(inputEmail.getText() == null){
-                errorMessage.setVisible(true);
-            }
-            else{
-                statement.setString(1, inputEmail.getText());
-                confirmed.setVisible(true);
-            }
-            statement.executeUpdate(input);
-        }
-        catch(SQLException e){
-            e.printStackTrace();
-        }
-        finally {
-            Cleaner.close(statement, null, connection);
-        }
-    }
-
-    public void genderConfirm(){
-
-        String input = "UPDATE Player SET gender = ? WHERE username = ?";
-
-        try{
-            connection = ConnectionPool.getConnection();
-
-            statement = connection.prepareStatement(input);
-            statement.setString(2,username);
-
-            if(chooseGender() == -1){
-                errorMessage.setVisible(true);
-            }
-            else{
-                statement.setInt(1, chooseGender());
-                confirmed.setVisible(true);
-            }
-            statement.executeUpdate(input);
-        }
-        catch(SQLException e){
-            e.printStackTrace();
-        }
-        finally {
-            Cleaner.close(statement, null, connection);
-        }
-    }
-
-    public void passwordConfirm(){
-
-        ResultSet rs = null;
-
-        String input = "UPDATE Player SET password = ? WHERE username = ?";
-        String sql = "SELECT password, salt FROM Player WHERE username = ?;";
-        try {
-            connection = ConnectionPool.getConnection();
-            statement = connection.prepareStatement(sql);
-            statement.setString(1, username);
-            rs = statement.executeQuery();
-
-            if (!(rs.next())) {
-                wrongPassword.setVisible(true);
-            }
-            else {
-                String salt = rs.getString("salt");
-                String realPassword = rs.getString("password");
-                String inputPassword = inputOldPassword.getText();
-
-                HashSalt hashedSaltedPass = new HashSalt();
-                byte[] byteSalt = hashedSaltedPass.decodeHexString(salt);
-                String hashedPassword = hashedSaltedPass.genHashSalted(inputPassword, byteSalt);
-
-                if (realPassword.equals(hashedPassword)) {
-
-                    statement = connection.prepareStatement(input);
-
-                    String inPassword = inputNewPassword.getText();
-                    HashSalt hashedSaltedPas = new HashSalt();
-                    salt1 = hashedSaltedPas.createSalt();
-                    stringSalt = hashedSaltedPass.encodeHexString(salt1);
-                    password = hashedSaltedPass.genHashSalted(inPassword, salt1);
-
-                    statement.setString(1, password);
-                    statement.setString(2, username);
-                    statement.executeUpdate();
-
-                    confirmed.setVisible(true);
-                } else {
-                    wrongPassword.setVisible(true);
-                }
-            }
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-        finally {
-
-        }
-    }
-
-    public int chooseGender(){
-        if(this.gender.getSelectedToggle().equals(this.btnMale)){
-            return 0;
-        }
-        else if(this.gender.getSelectedToggle().equals(this.btnFemale)){
-            return 1;
-        }
-        else return -1;
-    }
 
 }
