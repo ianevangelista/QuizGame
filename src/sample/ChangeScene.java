61e707eb9935e5b626ae45420c13d6fd609ed9a6
iaevange <iaevange@stud.ntnu.no>
2019-04-02 14:04:02 +0200

    checked connection and prepstatement

--- src/sample/ChangeScene.java
+++ src/sample/ChangeScene.java
@@ -5,9 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
 import java.io.IOException;
 
 
