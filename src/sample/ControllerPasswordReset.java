14ced5ab57583cac5ca67cff41f5e703ff283814
tirilbl <tirilbl@stud.ntnu.no>
2019-04-02 17:54:53 +0200

    Sender nytt passord på mail

--- src/sample/ControllerPasswordReset.java
+++ src/sample/ControllerPasswordReset.java
@@ -1,10 +1,16 @@
 package sample;
 
+import Connection.Cleaner;
+import Connection.ConnectionPool;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Random;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -13,7 +19,7 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
-import java.util.regex.Pattern;
+
 
 import static sample.ControllerHome.getUserName;
 
@@ -26,68 +32,46 @@ public class ControllerPasswordReset {
     MimeMessage message = new MimeMessage(session);
     Transport transport = null;
 
-    public void sendFeedback(){
+    private String passwordNew = newPassword();
 
-        String sendTo = "howdumbru.game@gmail.com";
-        String host = "smtp.gmail.com";
-        String username = "howdumbru.game@gmail.com";
-        String password = "Junierkul";
+    private Connection connection = null;
+    private PreparedStatement statement = null;
+    ResultSet rs = null;
 
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.user", username);
-        props.put("mail.smtp.password", password);
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.ssl.trust", host);
+    private byte[] salt;
+    private String stringSalt;
+    private String password;
 
-        try{
 
-            message.setFrom(new InternetAddress(username));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(sendTo));
-
-            message.setSubject("Feedback from How Dumb R U?");
-            String melding = email.getText() + " requests a new password.";
-            message.setText(melding);
-
-            transport = session.getTransport("smtp");
-            transport.connect(host, username, password);
-            transport.sendMessage(message, message.getAllRecipients());
-            transport.close();
-
-        }
-        catch (MessagingException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public void sendConfirm(){
+    public void sendPassword(){
 
         String sendTo = email.getText();
         String host = "smtp.gmail.com";
         String username = "howdumbru.game@gmail.com";
-        String password = "Junierkul";
+        String emailpassword = "Junierkul";
 
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.user", username);
-        props.put("mail.smtp.password", password);
+        props.put("mail.smtp.password", emailpassword);
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
         props.put("mail.smtp.ssl.trust", host);
+
 
         try{
 
             message.setFrom(new InternetAddress(username));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(sendTo));
 
-            message.setSubject("Password request recieved.");
-            message.setText("The request for a new password has been received! We will come back to you as soon as possible.\n" +
+            message.setSubject("New password");
+            message.setText("Your new password to 'How Dumb R U?': " +
+                    passwordNew +
+                    "\nRemember to change it as soon as possible!" +
                     "\n\nFrom: How Dumb R U?");
 
             transport = session.getTransport("smtp");
-            transport.connect(host, username, password);
+            transport.connect(host, username, emailpassword);
             transport.sendMessage(message, message.getAllRecipients());
             transport.close();
 
@@ -99,8 +83,10 @@ public class ControllerPasswordReset {
     }
 
     public void feedback(ActionEvent event){
-            sendFeedback();
-            sendConfirm();
+        if(validateEmail()){
+            sendPassword();
+            setPassword();
+        }
             ChangeScene.change(event, "Main.fxml");
     }
 
@@ -113,16 +99,61 @@ public class ControllerPasswordReset {
         }
     }
 
-    private boolean checkEmail(){
-        String getEmail = email.getText();
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
-                "[a-zA-Z0-9_+&*-]+)*@" +
-                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                "A-Z]{2,7}$";
 
-        Pattern pat = Pattern.compile(emailRegex);
-        if (getEmail == null)
-            return false;
-        return pat.matcher(getEmail).matches();
+    private String newPassword(){
+        Random ran = new Random();
+        int number = ran.nextInt(9000) + 1000;
+        return Integer.toString(number);
     }
+
+
+
+    public void setPassword(){
+        String input = "UPDATE Player SET password = ?, salt = ? WHERE email = ?";
+        try {
+            connection = ConnectionPool.getConnection();
+            statement = connection.prepareStatement(input);
+
+                HashSalt hashedSaltedPass = new HashSalt();
+                salt = hashedSaltedPass.createSalt();
+                stringSalt = hashedSaltedPass.encodeHexString(salt);
+                password = hashedSaltedPass.genHashSalted(passwordNew, salt);
+
+                statement.setString(1, password);
+                statement.setString(2, stringSalt);
+                statement.setString(3, email.getText());
+                statement.executeUpdate();
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            Cleaner.close(statement, rs, connection);
+        }
+    }
+
+
+    public boolean validateEmail() {
+        String sql = "SELECT username FROM Player WHERE email = ?;";
+        try {
+            connection = ConnectionPool.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, email.getText());
+            rs = statement.executeQuery();
+
+            //if user does not exist
+            if (!(rs.next())) {
+                return false;
+            } else {
+                return true;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }finally{
+            Cleaner.close(statement, rs, connection);
+        }
+    }
+
 }
