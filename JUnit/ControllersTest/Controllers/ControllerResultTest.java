69dcfeb5a93881e119c51c6e66239a745f42e66e
junill <junill@stud.ntnu.no>
2019-04-05 14:00:17 +0200

    Merge branch 'development' of https://gitlab.stud.idi.ntnu.no/tirilbl/tdat1006 into development
    # Conflicts:
    #	src/sample/ControllerGame.java

---
+++ JUnit/ControllersTest/Controllers/ControllerResultTest.java
@@ -0,0 +1,32 @@
+package Controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerResultTest {
+
+    @Test
+    public void initialize() {
+    }
+
+    @Test
+    public void timerRes() {
+    }
+
+    @Test
+    public void checkRes() {
+    }
+
+    @Test
+    public void turnOfTimerR() {
+    }
+
+    @Test
+    public void addGamesLost() {
+    }
+
+    @Test
+    public void addGamesWon() {
+    }
+}
