15ae9c64723c0809e3dc7ce3fea7c6522f06388f
Emir Derouiche <memir0@hotmail.no>
2019-04-11 13:30:20 +0200

    remove back button from question answer page

--- src/Controllers/ControllerQuestion.java
+++ src/Controllers/ControllerQuestion.java
@@ -224,10 +224,6 @@ public class ControllerQuestion {
             connection = ConnectionPool.getConnection();
             statement = connection.createStatement();
             String sqlPlayer = "SELECT player1 FROM Game WHERE gameId=" + getGameId() + ";";
-
-            //TODO: Delete this
-            System.out.println(sqlPlayer);
-
             rs = statement.executeQuery(sqlPlayer);
             if(rs.next()) {
                 String player1Name = rs.getString("player1");
@@ -278,60 +274,6 @@ public class ControllerQuestion {
             timer.cancel();
             timer.purge();
             return;
-        }
-    }
-
-
-    //Home-button quits game
-    public boolean sceneHome(ActionEvent event) {
-        // Connection objects
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet rs = null;
-
-        try {
-            connection = ConnectionPool.getConnection();
-            statement = connection.createStatement();
-
-            //Automatically lose a game if you log out
-            int gameId = getGameId();
-            if(gameId != 0){
-                String player = findUser();
-                String sqlRageQuitGame;
-                String rageQuittingPlayer = player.equals("player1") ? "p1Points = 0, p1Finished = 1" : "p2Points = 0, p2Finished = 1";
-                sqlRageQuitGame = "UPDATE Game SET " + rageQuittingPlayer + " WHERE gameId =" + gameId + ";";
-                statement.executeUpdate(sqlRageQuitGame);
-
-                //remove gameId from player
-                String sqlRemoveGameId = "UPDATE Player SET gameId = NULL, gamesLost = gamesLost+1 WHERE username = '" + username + "';";
-                statement.executeUpdate(sqlRemoveGameId);
-
-                //Delete game if other player is finished and give opponent points
-                String sqlCheckIfOtherPlayerHasLeft = "SELECT gameId FROM Player WHERE gameId =" + gameId + ";";
-                ResultSet rsPlayersWithTheGameId = statement.executeQuery(sqlCheckIfOtherPlayerHasLeft);
-
-                if (!rsPlayersWithTheGameId.next()) {
-                    String points = player.equals("player1") ? "p2Points" : "p1Points";
-                    String opponent = player.equals("player1") ? "player2" : "player1";
-                    String getOpponentPoints = "SELECT player2, p2Points FROM Game WHERE gameId = " + gameId;
-                    rs = statement.executeQuery(getOpponentPoints);
-                    rs.next();
-                    int opponentPoints = rs.getInt(points);
-                    String sqlUpdatePlayerScore = "UPDATE Player SET points= points +" + opponentPoints + " WHERE username ='" + rs.getString(opponent) + "';";
-                    statement.executeUpdate(sqlUpdatePlayerScore);
-
-                    String sqlDeleteGame = "DELETE FROM Game WHERE gameId =" + gameId + ";";
-                    statement.executeUpdate(sqlDeleteGame);
-                    resetGameId();
-                }
-            }
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }finally {
-            Cleaner.close(statement, null, connection);
-            ChangeScene.change(event, "/Scenes/Game.fxml");
         }
     }
 }
